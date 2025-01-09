280ed7e80d33f6d54934ffbbee54868717b33f84
Gabriel <gabriel@swispace.com>
2025-01-09 12:50:44 +0100

    fix: tribe card layout

--- src/components/tribe/Tribe.tsx
+++ src/components/tribe/Tribe.tsx
@@ -7,26 +7,31 @@ import { IconArrowRight } from "@tabler/icons-react";
 const Tribe: FC<TribeProps> = ({ ...props }) => {
   return (
     <div className={"socraft-tribe-card" + (props.darkMode ? " dark" : "")}>
-      <div
-        className="tribe-image"
-        style={{ backgroundImage: `url(${props.image})` }}
-      />
-      <div className="content">
-        <div className="header">
-          <h2 className="title">{props.name}</h2>
-          <span className="members">{props.members} experts</span>
+      <div className="header">
+        <div
+          className="tribe-image"
+          style={{ backgroundImage: `url(${props.image})` }}
+        />
+        <div className="text">
+          <div className="title-container">
+            <h2 className="title">{props.name}</h2>
+            <span className="members">
+              {props.members}{" "}
+              {props.members > 1 ? "crafters experts" : "crafter expert"}
+            </span>
+          </div>
+          <p className="description">{props.description}</p>
         </div>
-        <p className="description">{props.description}</p>
-        <div className="actions">
-          <Button
-            variant="outlined"
-            darkMode={props.darkMode}
-            endIcon={<IconArrowRight />}
-            onClick={() => window.open(`/${props.slug}`, "_self")}
-          >
-            Voir plus
-          </Button>
-        </div>
+      </div>
+      <div className="actions">
+        <Button
+          variant="outlined"
+          darkMode={props.darkMode}
+          endIcon={<IconArrowRight />}
+          onClick={() => window.open(`/${props.slug}`, "_self")}
+        >
+          Voir plus
+        </Button>
       </div>
     </div>
   );
