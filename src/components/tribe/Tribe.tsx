74ab8c017b7f3e3b29aefe1d5eaa219c9f219ed0
Gabriel <gabriel@swispace.com>
2025-01-09 11:04:12 +0100

    feat: tribe card component

---
+++ src/components/tribe/Tribe.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from "react";
+import { TribeProps } from "./Tribe.types";
+import "./Tribe.css";
+import { Button } from "../button";
+import { IconArrowRight } from "@tabler/icons-react";
+
+const Tribe: FC<TribeProps> = ({ ...props }) => {
+  return (
+    <div className={"socraft-tribe-card" + (props.darkMode ? " dark" : "")}>
+      <div
+        className="image"
+        style={{ backgroundImage: `url(${props.image})` }}
+      />
+      <div className="content">
+        <div className="header">
+          <h2 className="title">{props.name}</h2>
+          <span className="members">{props.members} membres</span>
+        </div>
+        <p className="description">{props.description}</p>
+        <div className="actions">
+          <Button
+            variant="outlined"
+            darkMode={props.darkMode}
+            endIcon={<IconArrowRight />}
+            onClick={() => window.open(`/${props.slug}`, "_self")}
+          >
+            Voir plus
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tribe;
---
+++ src/components/tribe/index.ts
@@ -0,0 +1,1 @@
+export { default as Tribe } from "./Tribe";
