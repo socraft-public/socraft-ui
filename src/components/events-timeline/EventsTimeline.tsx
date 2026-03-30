2b6a96909ccbe8ffce9e448e74a55216721060bb
Gabriel <gabriel@optifit.app>
2026-03-30 16:01:40 +0200

    fix: avoid reversing events order

--- src/components/events-timeline/EventsTimeline.tsx
+++ src/components/events-timeline/EventsTimeline.tsx
@@ -27,51 +27,49 @@ export default function EventsTimeline({
   return (
     <div className="lg:max-w-screen mt-3">
       <div className="relative">
-        {[...events]
-          .reverse()
-          .map(({ summary, description, start, location }, index) => (
-            <div key={index} className="group relative">
-              <div className="flex items-start">
-                <div className="mt-3 mr-5 flex flex-col gap-2 shrink-0 w-[45px] sm:w-[130px] text-end">
-                  <h6 className="text-sm text-[#fbbb10] font-semibold">
-                    {formatDate(start.dateTime || start.date || "", locale)}
-                  </h6>
-                  <span className="text-xs sm:text-sm text-muted-foreground">
-                    {differenceFromNow(
-                      start.dateTime
-                        ? new Date(start.dateTime).getTime()
-                        : start.date
-                          ? new Date(start.date).getTime()
-                          : NaN,
-                      start.date,
-                      locale,
-                    )}
-                  </span>
-                </div>
-                <div className="relative pb-10 border-l-2 dark:border-l-white/20 group-last:pb-35 pl-6 sm:pl-8 space-y-2">
-                  <div className="absolute h-3 w-3 -translate-x-1/2 -left-px top-4 rounded-full border-2 border-[#fbbb10] bg-background" />
-                  <h3 className="text-lg sm:text-xl font-semibold mb-0 dark:text-white">
-                    {summary}
-                  </h3>
-                  {expanded && (
-                    <>
-                      {location && (
-                        <h4 className="text-sm font-medium text-foreground/40">
-                          {location}
-                        </h4>
-                      )}
-                      <p
-                        className="text-sm sm:text-base text-muted-foreground"
-                        dangerouslySetInnerHTML={{
-                          __html: description ?? "Bientôt plus d'infos...",
-                        }}
-                      />
-                    </>
+        {[...events].map(({ summary, description, start, location }, index) => (
+          <div key={index} className="group relative">
+            <div className="flex items-start">
+              <div className="mt-3 mr-5 flex flex-col gap-2 shrink-0 w-[45px] sm:w-[130px] text-end">
+                <h6 className="text-sm text-[#fbbb10] font-semibold">
+                  {formatDate(start.dateTime || start.date || "", locale)}
+                </h6>
+                <span className="text-xs sm:text-sm text-muted-foreground">
+                  {differenceFromNow(
+                    start.dateTime
+                      ? new Date(start.dateTime).getTime()
+                      : start.date
+                        ? new Date(start.date).getTime()
+                        : NaN,
+                    start.date,
+                    locale,
                   )}
-                </div>
+                </span>
+              </div>
+              <div className="relative pb-10 border-l-2 dark:border-l-white/20 group-last:pb-35 pl-6 sm:pl-8 space-y-2">
+                <div className="absolute h-3 w-3 -translate-x-1/2 -left-px top-4 rounded-full border-2 border-[#fbbb10] bg-background" />
+                <h3 className="text-lg sm:text-xl font-semibold mb-0 dark:text-white">
+                  {summary}
+                </h3>
+                {expanded && (
+                  <>
+                    {location && (
+                      <h4 className="text-sm font-medium text-foreground/40">
+                        {location}
+                      </h4>
+                    )}
+                    <p
+                      className="text-sm sm:text-base text-muted-foreground"
+                      dangerouslySetInnerHTML={{
+                        __html: description ?? "Bientôt plus d'infos...",
+                      }}
+                    />
+                  </>
+                )}
               </div>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </div>
   );
